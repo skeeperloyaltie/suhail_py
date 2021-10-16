b96038caa236307dfa143fd2dd9b6fc239e36c88
skeeperloyaltie <gugod254@gmail.com>
2021-10-16 04:34:41 +0300

    Added UmL and changed values in py file

--- hospital.py
+++ hospital.py
@@ -7,6 +7,8 @@ global address
 global phone
 global type_of_patient
 global department
+global employee_id
+global salary
 
 name = str(input("Enter name: "))
 gender = str(input("Enter gender: "))
@@ -14,6 +16,8 @@ address = str(input("Enter address: "))
 phone = str(input("Enter phone: "))
 department = str(input("Enter department: "))
 type_of_patient = str(input("Enter type_of_patient: "))
+employee_id = int(input("Enter employee_id: "))
+salary = float(input("Enter salary: "))
 
 class hospital:
     
@@ -22,7 +26,6 @@ class hospital:
 
 class employees(hospital):
     def __init__(self, employee_id, salary):
-        
         self.employee_id = employee_id
         self.salary = salary
 class patients(hospital):
@@ -33,27 +36,34 @@ class patients(hospital):
         self.phone = phone
         self.type_of_patient = type_of_patient
     def patie(self):
-        
         print("Patient name is " + name + " with gender " + gender + " and lives in " + address + " with phone " + phone + " and is of type " + type_of_patient)
-
 class doctors(employees):
-    def __init__(self, name, gender, address, phone, department):
+    def __init__(self, name, gender, address, phone, department, employee_id, salary):
         self.name = name
         self.gender = gender
         self.address = address
         self.phone = phone
         self.department = department
+        self.employee_id = employee_id
+        self.salary = salary
     def duct(self):
         
-        print("\n Doctors name: " + name + "\n Gender: " + gender + "\n Address: " + address + "\nPhone: " + phone + "\nDepartment: " + department)
+        print("Doctor ID: " + employee_id + "\n Doctors name: " + name + "\n Gender: " + gender + "\n Address: " + address + "\nPhone: " + phone + "\nDepartment: " + department + "\nSalary: " + salary)
    
 class nurses(employees):
-    def __init__(self, name, gender, address, phone, department):
+    def __init__(self, name, gender, address, phone, department, employee_id, salary):
         self.name = name
         self.gender = gender
         self.address = address
         self.phone = phone
         self.department = department
+        self.employee_id = employee_id
+        self.salary = salary
+
+    def duct(self):
+        print("Nurse ID: " + employee_id + "\n Nurse name: " + name + "\n Gender: " + gender + "\n Address: " +
+              address + "\nPhone: " + phone + "\nDepartment: " + department + "\nSalary: " + salary)
+
 
 # Menu for input
 def main():
@@ -62,8 +72,9 @@ def main():
     if choice == 1:
         print("Enter Doctors information: ")
         # Enter object values
-        doctor1 = doctors(name, gender, address, phone, department)
+        doctor1 = doctors(name, gender, address, phone, department, employee_id, salary)
         doctor1.duct()
+
     elif choice == 2:
         print("Enter patients information information: ")
         patient1 = patients(name, gender, address, phone, type_of_patient)
@@ -73,16 +84,12 @@ def main():
     elif choice == 3:
         print("Enter patients information information: ")
         # Enter object values
-        doctor1 = doctors(name, gender, address, phone)
-        doctor1.duct()
+        nurse1 = doctors(name, gender, address, phone, department, employee_id, salary)
+        nurse1.duct()
     else:
         print("Wrong choice: ")
         return main()
     
-
-
-
-   
 
 
 print(main())
