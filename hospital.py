c55e1195d202d38625eb23c738decf4e5749ede5
skeeperloyaltie <gugod254@gmail.com>
2021-10-16 03:36:30 +0300

    menu updated

--- hospital.py
+++ hospital.py
@@ -1,11 +1,27 @@
 # Question Two
 # Hospital System 
+
+global name
+global gender
+global address
+global phone
+global type_of_patient
+global department
+
+name = str(input("Enter name: "))
+gender = str(input("Enter gender: "))
+address = str(input("Enter address: "))
+phone = str(input("Enter phone: "))
+department = str(input("Enter department: "))
+type_of_patient = str(input("Enter type_of_patient: "))
 class hospital:
+    
     def __init__(self):
         print("This is the hospital Sys")      
 
 class employees(hospital):
     def __init__(self, employee_id, salary):
+        
         self.employee_id = employee_id
         self.salary = salary
 class patients(hospital):
@@ -16,11 +32,7 @@ class patients(hospital):
         self.phone = phone
         self.type_of_patient = type_of_patient
     def patie(self):
-        name = values.name
-        gender = values.gender
-        address = values.address
-        phone = values.phone
-        type_of_patient = values.type_of_patient
+        
         print("Patient name is " + name + " with gender " + gender + " and lives in " + address + " with phone " + phone + " and is of type " + type_of_patient)
 
 class doctors(employees):
@@ -31,11 +43,7 @@ class doctors(employees):
         self.phone = phone
         self.department = department
     def duct(self):
-        name = values.name
-        gender = values.gender
-        address = values.address
-        phone = values.phone
-        department = values.department
+        
         print("\n Doctors name: " + name + "\n Gender: " + gender + "\n Address: " + address + "\nPhone: " + phone + "\nDepartment: " + department)
    
 class nurses(employees):
@@ -46,42 +54,34 @@ class nurses(employees):
         self.phone = phone
         self.department = department
 
-
 # Menu for input
-def values(self):
-    name = str(input("Enter name: "))
-    gender = str(input("Enter gender: "))
-    address = str(input("Enter address: "))
-    phone = str(input("Enter phone: "))
-    department = str(input("Enter department: "))
-    type_of_patient = str(input("Enter type_of_patient: "))
+def main():
+    choice = int(input("Choose menu number [1, 2, 3] to determine input category: "))
 
-def menu():
-    x = print(int(input("Choose menu number to determine input category: Ex: 1, 2, 3")))
-    name = values.name 
-    gender = values.gender
-    address = values.address
-    phone = values.phone
-    type_of_patient = values.type_of_patient
-    if x == 1:
+    if choice == 1:
         print("Enter Doctors information: ")
-        
-        department = str(input("Enter department: "))
-        
         # Enter object values
-        doctor1 = menu(doctors(name, gender, address, phone))
+        doctor1 = doctors(name, gender, address, phone, department)
         doctor1.duct()
-    elif x == 2:
+    elif choice == 2:
         print("Enter patients information information: ")
         patient1 = patients(name, gender, address, phone, type_of_patient)
         patient1.patie()
 
     # Enter data for nurses
-    elif x == 3:
+    elif choice == 3:
         print("Enter patients information information: ")
         # Enter object values
         doctor1 = doctors(name, gender, address, phone)
         doctor1.duct()
+    else:
+        print("Wrong choice: ")
+        return main()
+
+print(main())
+
+
+
 
 
 
